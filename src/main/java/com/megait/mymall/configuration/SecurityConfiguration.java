9e40866b39c404393419eeb88c5993651ebbb532
Miri Lee <dnls2002@naver.com>
2021-08-31 22:59:22 +0900

    이메일인증, 자동로그인

--- src/main/java/com/megait/mymall/configuration/SecurityConfiguration.java
+++ src/main/java/com/megait/mymall/configuration/SecurityConfiguration.java
@@ -2,19 +2,30 @@ package com.megait.mymall.configuration;
 // 시큐리티 관련 Configuration 빈
 
 import com.megait.mymall.service.CustomOAuth2UserService;
+import com.megait.mymall.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+
+    private final MemberService memberService;
+    private final DataSource dataSource; // DBCP (애플리케이션 프로퍼티에 H2설정했던거)
 
 
     // 웹 관련 보안 설정 (예). 방화벽)
@@ -66,12 +77,37 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         .logoutUrl("/logout") // 로그아웃을 실행할 url. 디폴트는 "/logout"
                         .invalidateHttpSession(true) // 로그아웃 완료 시 세션을 종료시킬 것인가.
                         .logoutSuccessUrl("/")
+                .and()
+                        .rememberMe()
+                            .userDetailsService(memberService)
+                            .tokenRepository(tokenRepository()) // 토큰레파지토리 우리가 만든거
+                        // 토큰레파지토리 - RememberMe를 저장할 테이블
+
 
                 .and()
                         .oauth2Login()
                         .loginPage("/login")
                         .userInfoEndpoint()
                         .userService(customOAuth2UserService)
+
         ;
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository(){
+        // PersistentTokenRepository : RememberMe 선택한 사용자들의 토큰값을 관리한다.
+        // private은 빈이 될 수 없음 퍼블릭으로 하셈
+
+        JdbcTokenRepositoryImpl jdbcTokenRepository =
+                new JdbcTokenRepositoryImpl();
+
+        // JdbcTokenRepositoryImpl 들어가보면 이미 sql 쿼리가 지정되어있음
+        // 필드이름 봐두기
+
+        jdbcTokenRepository.setDataSource(dataSource);
+        // 내가 연결시켜놓은 h2데이터소스를 집어넣는거임
+
+        return jdbcTokenRepository;
+
     }
 }
--- src/main/java/com/megait/mymall/service/EmailService.java
+++ src/main/java/com/megait/mymall/service/EmailService.java
@@ -3,15 +3,20 @@ package com.megait.mymall.service;
 import com.megait.mymall.domain.Member;
 import com.megait.mymall.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService {
 
     private final JavaMailSender javaMailSender;
@@ -26,9 +31,43 @@ public class EmailService {
         // TOKEN 을 DB에 update
         member.setEmailCheckToken(token);
 
-        // 이메일 날리기
+        // sendSimpleMailMessage(member);
+        sendHtmlMailMessage(member);
+
+
+    }
+
+    private void sendHtmlMailMessage(Member member) {
+        // 이메일 날리기 (html으로 꾸민 버전)
+        String html = "<html><body>" +
+                "<p style=\"background:red\">링크 : <a href=\"http://localhost:8080/email-check?email=" + member.getEmail()
+                + "&token=" + member.getEmailCheckToken() + "\">이메일 인증을 원하시는 경우 이곳을 클릭하세요.</a></p>" +
+                "</body></html>";
+
+        // html 페이지를 보낼때는 일반 메세지가 아니라, MIME 타입의 메세지를 보내야함
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper =
+                    new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            // false = 멀티포트 (파일 다운로드도 같이해야하니?)
+
+            String title = "[My Mall] 회원 가입에 감사드립니다. 딱 한 가지 과정이 남았습니다!";
+            mimeMessageHelper.setTo(member.getEmail());
+            mimeMessageHelper.setSubject(title);
+            mimeMessageHelper.setText(html, true);
+            // true라고 써줘야지 반드시 html으로 날아간다.
+
+            javaMailSender.send(mimeMessage);
+
+        }catch (MessagingException e){
+            log.error("이메일 전송 실패. ({})", member.getEmail());
+        }
+    }
+
+    private void sendSimpleMailMessage(Member member){
+        // 이메일 날리기 (간단한버전)
         String url = "http://localhost:8080/email-check?email="+member.getEmail()
-                    +"&token="+token;
+                +"&token="+member.getEmailCheckToken();
         String title = "[My Mall] 회원 가입에 감사드립니다. 딱 한 가지 과정이 남았습니다!";
         String message ="다음 링크를 브라우저에 붙여넣어주세요. 링크 : " + url;
         String sender = "mymall-admin-norely@mymall.com";
@@ -39,6 +78,5 @@ public class EmailService {
         mailMessage.setText(message); // 본문
         mailMessage.setFrom(sender);
         javaMailSender.send(mailMessage);
-
     }
 }
